422b850dc9040d8556c532cbbf4b51657adaa1c8
LuizAndrade <luizhfbandrade@gmail.com>
2016-09-02 22:21:25 -0300

    Delete main.lua

--- trabalhos/trabalhos-02/main.lua
+++
@@ -1,7 +0,0 @@
-function love.load(arg)
-  if arg[#arg] == "-debug" then require("mobdebug").start() end 
-  -- utilizando esse comando pois estou usando ZeroBrane Studio para debuggar o programa
-end
-function love.draw()
-  love.graphics.print("Hello World", 100, 100)
-end
